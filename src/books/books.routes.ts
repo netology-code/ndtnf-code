3165ee7e001f311e74e673e7766735dcc436de01
Alex Kulagin <al.qlagin@gmail.com>
2021-02-20 14:39:18 +0500

    fix any

--- src/books/books.routes.ts
+++ src/books/books.routes.ts
@@ -1,15 +1,16 @@
 import { Router } from "express";
 import { container } from "../container";
+import { BooksService } from './books.service';
 const router = Router();
 
 router.get("", async (req, res) => {
-  const service: any = container.get("BOOKS_SERVICE");
+  const service: BooksService = container.get("BOOKS_SERVICE");
   const books = await service.findAll();
   res.json(books);
 });
 
 router.post("", async (req, res) => {
-  const service: any = container.get("BOOKS_SERVICE");
+  const service: BooksService = container.get("BOOKS_SERVICE");
   const book = service.create(req.body);
   res.json(book);
 });
--- src/books/books.service.ts
+++ src/books/books.service.ts
@@ -1,11 +1,17 @@
 import { Book } from "./books.model";
-
+interface CreateBookDto {
+  title: string;
+  description: string;
+  authors: string[];
+  favorite: string;
+  fileCover: string;
+}
 export class BooksService {
   constructor() {
     console.log("new BooksService");
   }
 
-  async create(data: any) {
+  async create(data: CreateBookDto) {
     const book = new Book(data);
     await book.save();
     return book;
