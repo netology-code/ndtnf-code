be75eb1c1b09bd792d5a4607e0b4a3a783fcb75e
Alex Kulagin <al.qlagin@gmail.com>
2021-02-20 14:49:13 +0500

    use class to provide BooksService

--- src/books/books.routes.ts
+++ src/books/books.routes.ts
@@ -4,13 +4,13 @@ import { BooksService } from './books.service';
 const router = Router();
 
 router.get("", async (req, res) => {
-  const service: BooksService = container.get("BOOKS_SERVICE");
+  const service = container.get(BooksService);
   const books = await service.findAll();
   res.json(books);
 });
 
 router.post("", async (req, res) => {
-  const service: BooksService = container.get("BOOKS_SERVICE");
+  const service = container.get(BooksService);
   const book = service.create(req.body);
   res.json(book);
 });
--- src/container.ts
+++ src/container.ts
@@ -3,4 +3,4 @@ import { BooksService } from "./books/books.service";
 export const container = new Container();
 
 decorate(injectable(), BooksService);
-container.bind("BOOKS_SERVICE").to(BooksService).inSingletonScope();
+container.bind(BooksService).toSelf().inSingletonScope();
